b7d6967f4057d3d22ce451cc446404fb089b4202
CasaN0va-bhavit <bhavit.grover@outlook.com>
2024-07-07 01:27:02 +0530

    made frontend thoda bohot

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,26 +1,76 @@
-import { Text, View, Button, Alert, StyleSheet } from "react-native";
+import { Text, View, Pressable, Alert, StyleSheet, Button } from "react-native";
+import React, { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFonts ,Outfit_400Regular, Outfit_700Bold } from '@expo-google-fonts/outfit';
 
 export default function Home({navigation}) {
+  
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
+  let [fontsLoaded] = useFonts({
+    Outfit_400Regular,
+    Outfit_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
+  async function fetchDataFromAsyncStorage() {
+    const storedEmail = await AsyncStorage.getItem('Email');
+    const storedPass = await AsyncStorage.getItem('Password');
+  
+    console.log("the stored email and pass is: ", storedEmail, storedPass)
+    setEmail(storedEmail)
+    setPass(storedPass)
+  }
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text style={styles.heading}>Home</Text>
-      <Button
-        title="Login"
+      <Text style={styles.heading}>Hi there! 👋</Text>
+      <Pressable 
+        style={styles.button}
         onPress={() => navigation.navigate('Login')}
-      />
+      >
+        <Text style={styles.buttonTxt}>Login</Text>
+        </Pressable>
+      {/* <Button
+        title="Fetch"
+        onPress={() => fetchDataFromAsyncStorage()}
+      /> */}
     </View>
   );
 }
 
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   heading: {
-    fontSize: 20
+    fontFamily: "Outfit_700Bold",
+    fontSize: 40,
+    marginBottom: 50
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  buttonTxt: {
+    fontSize: 16,
+    lineHeight: 21,
+    letterSpacing: 0.25,
+    color: 'white',
+    fontFamily: "Outfit_400Regular"
   }
 })
--- app/(tabs)/login.jsx
+++ app/(tabs)/login.jsx
@@ -1,10 +1,44 @@
-import { Text, View, TextInput, StyleSheet, Button } from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Dashboard from "./dashboard";
+import React, { useState } from 'react';
+import { Text, View, TextInput, StyleSheet, Pressable } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function Login({navigation}) {
+
+  const [error, setError] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function handleSubmit() {
+    if (!email || !password) {
+      setError("Please enter all the details.")
+      return
+    }
+    
+    if (!email.includes("@")) {
+      setError("Please enter a valid email.")
+      return
+    }
+
+    console.log(email)
+    console.log(password)
+
+    try {
+      await AsyncStorage.setItem(
+        'Email',
+        email,
+      );
+      await AsyncStorage.setItem(
+        'Password',
+        password,
+      );
+    } catch (error) {
+      // Error saving data
+      console.log('An error occured in asyncstorage: ' + error)
+    }
+    navigation.navigate('Dashboard')
+  }
+
   return (
     <View
       style={{
@@ -15,21 +49,61 @@ export default function Login({navigation}) {
     >
     <Text style={styles.heading}>Login</Text>
     <TextInput 
-    placeholder="Email" />
-    <TextInput 
-    secureTextEntry={true}
-    placeholder="Password" />
-    <Button
-      title="Go home"
-      onPress={() => navigation.navigate('Home')}
+    style={styles.input}
+    placeholder="Email" 
+    onChangeText={(newText) => setEmail(newText)}
     />
+    <TextInput 
+    style={styles.input}
+    secureTextEntry={true}
+    onChangeText={(newText) => setPassword(newText)}
+    placeholder="Password" />
+    <Text style={styles.error}>{error}</Text>
+    <Pressable 
+        style={styles.button}
+        onPress={() => handleSubmit()}
+      >
+        <Text style={styles.buttonTxt}>Submit</Text>
+        </Pressable>
     </View>
   
   );
 }
 
 const styles = StyleSheet.create({
-  heading : {
-    fontSize: 30
+  heading: {
+    fontFamily: "Outfit_700Bold",
+    fontSize: 40,
+    marginBottom: 30
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+    marginTop: 20
+  },
+  buttonTxt: {
+    fontSize: 16,
+    lineHeight: 21,
+    letterSpacing: 0.25,
+    color: 'white',
+    fontFamily: "Outfit_400Regular"
+  },
+  input: {
+    // borderColor: "#000"
+    borderColor: "#000",
+    width: "60%",
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    marginBottom: 10,
+    fontFamily: "Outfit_400Regular"
+  },
+  error: {
+    fontFamily: "Outfit_400Regular"
   }
 })
